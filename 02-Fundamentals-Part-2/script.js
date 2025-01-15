2aa858563977a7c142b4c7fe456b0be6f06d2c6c
Anil Kunda <anilkunda701@gmail.com>
2025-01-15 21:41:47 +0530

    loops in loops

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -338,3 +338,13 @@ const anil2 = [
 for (let i = anil2.length - 1; i >= 0; i--) {
   console.log(i, anil2[i]);
 }
+
+// ----- Loops in Loops
+
+for (let exercise = 1; exercise < 3; exercise++) {
+  console.log(`--Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weights repetition ${rep}`);
+  }
+}
